1a5176bd91a8f9a55703938c7ec076b08f690ace
psullivan2213 <96510015+psullivan2213@users.noreply.github.com>
2025-11-15 16:09:58 -0500

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -4,9 +4,9 @@ export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const IS_WORKFLOW = `${process.env.NEXT_PUBLIC_APP_TYPE_WORKFLOW}` === 'true'
 export const APP_INFO: AppInfo = {
-  title: 'Text Generator APP',
+  title: 'AI-World-Fitness-Trainer',
   description: 'App description',
-  copyright: '',
+  copyright: 'AI-World',
   privacy_policy: '',
   default_language: 'en-US',
 }
